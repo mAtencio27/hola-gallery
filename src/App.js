0782ebb13d5fdaf1b025569a7905c106ce8d884f
mAtencio27 <mAtencio27@hotmail.com>
2022-05-26 20:43:40 +0900

    apollo setup to work with the api. Created component folder along with the GetNft file to quary the data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from '@apollo/client';
+import {onError} from '@apollo/client/link/error'
+
+const errorLink = onError(({graphqlErrors, networkError}) => {
+  if(graphqlErrors) {
+    graphqlErrors.map(({ message, location, path}) => {
+      alert(`GraphQL error ;( ${message}`);
+    });
+  }
+});
+
+//this is where we put the api path
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "https://graph.holaplex.com/v1"}),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and poop to reload.
+          </p>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
